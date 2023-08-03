424278b1ccbecbee139ac17dd9c3f304ce4dca2b
Arnas Bradauskas <arnas.bradauskas@traveltime.com>
2023-08-03 18:38:07 +0300

    Made png prettier, added dynamic cell sizes

--- src/main/scala/com/traveltime/tablePngConverter/Main.scala
+++ src/main/scala/com/traveltime/tablePngConverter/Main.scala
@@ -1,21 +1,31 @@
 package com.traveltime.tablePngConverter
 
-import java.awt.{Color, Font}
+import java.awt.{Color, Font, Graphics2D}
 import java.awt.image.BufferedImage
 import javax.imageio.ImageIO
 import java.io.File
-import scala.util.{Try, Failure, Success}
+import scala.util.{Failure, Success, Try}
 
 object Main {
   private def createTableImage(
       table: List[List[String]]
-  ): Try[BufferedImage] =
-    Try {
-      val cellWidth = 100
-      val cellHeight = 30
-      val font = new Font("Arial", Font.PLAIN, 12)
+  ): Try[BufferedImage] = {
+    val font = new Font("Courier", Font.PLAIN, 32)
+    val headerFont = font.deriveFont(Font.BOLD)
 
-      val imageWidth = cellWidth * table.head.size
+    Try {
+      val gTemp =
+        new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB).createGraphics()
+      gTemp.setFont(font)
+      val metrics = gTemp.getFontMetrics(font)
+
+      // Calculate cell dimensions for each column
+      val columnWidths = table.transpose.map(column =>
+        column.map(cell => metrics.stringWidth(cell)).max + 20
+      )
+      val cellHeight = metrics.getHeight + 10
+
+      val imageWidth = columnWidths.sum
       val imageHeight = cellHeight * table.size
 
       val bufferedImage =
@@ -25,25 +35,50 @@ object Main {
       g.setFont(font)
       g.setColor(Color.white)
       g.fillRect(0, 0, imageWidth, imageHeight)
-      g.setColor(Color.black)
 
-      table.zipWithIndex.flatMap { case (row, i) =>
+      val cellOps = table.zipWithIndex.flatMap { case (row, i) =>
         row.zipWithIndex.map { case (cell, j) =>
-          val x = j * cellWidth
+          val x = columnWidths.slice(0, j).sum
           val y = i * cellHeight
-          g.drawRect(x, y, cellWidth, cellHeight)
-          g.drawString(cell, x + 5, y + (cellHeight / 2))
+          val cellWidth = columnWidths(j)
+
+          (g: Graphics2D) => {
+            // Color alternate rows
+            if (i % 2 == 1) {
+              g.setColor(new Color(235, 235, 235))
+              g.fillRect(x, y, cellWidth, cellHeight)
+            }
+
+            // Draw cell border
+            g.setColor(Color.black)
+            g.drawRect(x, y, cellWidth, cellHeight)
+
+            // Use header font for first row
+            if (i == 0) {
+              g.setFont(headerFont)
+              g.setColor(Color.blue)
+            } else {
+              g.setFont(font)
+              g.setColor(Color.black)
+            }
+
+            // Adjust text placement in the cell
+            g.drawString(cell, x + 10, y + cellHeight - 10)
+          }
         }
       }
+
+      cellOps.foreach(op => op(g))
 
       g.dispose()
 
       bufferedImage
     }
+  }
 
   def main(args: Array[String]): Unit = {
     val table = List(
-      List("", "column1", "column2"),
+      List("", "column1111111111111111111111", "column2"),
       List("row1", "value", "value"),
       List("row2", "value", "value")
     )
